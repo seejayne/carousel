2e41639239f93f60e4f5804f58de26cbcb8dbb37
seejayne <jaynev@gmail.com>
2014-09-18 16:50:04 -0700

    Created new repo
    For Carousel. One project per repo this time!

---
+++ carousel/IntroViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IntroViewController.swift
+//  carousel
+//
+//  Created by Jayne Vidheecharoen on 9/18/14.
+//  Copyright (c) 2014 Jayne Vidheecharoen. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  carousel
+//
+//  Created by Jayne Vidheecharoen on 9/18/14.
+//  Copyright (c) 2014 Jayne Vidheecharoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
