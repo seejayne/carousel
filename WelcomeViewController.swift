8b5dab2db58c8ccd46570f0d2d6cac0af552b91e
seejayne <jaynev@gmail.com>
2014-09-18 22:47:26 -0700

    paging welcome screens

---
+++ WelcomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  WelcomeViewController.swift
+//  carousel
+//
+//  Created by Jayne Vidheecharoen on 9/18/14.
+//  Copyright (c) 2014 Jayne Vidheecharoen. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSize(width: 1280, height: 518)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+        // Get the current page based on the scroll offset
+        var page : Int = Int(round(scrollView.contentOffset.x / 320))
+        
+        // Set the current page, so the dots will update
+        pageControl.currentPage = page
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
